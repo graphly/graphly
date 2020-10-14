f97071bcc5f1babd77d2ab72e27d736ec08505b2
mrRachar <matthewross@campars.net>
2020-10-14 22:14:27 +0100

    Fix XMLSimWriter name

--- src/main/scala/io/XMLSimWriter.scala
+++ src/main/scala/io/XMLSimWriter.scala
@@ -2,10 +2,10 @@ package io
 
 import model.sim.Sim
 
-object XMLGraphWriter extends SimWriter[xml.Elem] {
+object XMLSimWriter extends SimWriter[xml.Elem] {
   override def write(x: Sim): xml.Elem = {
     xml.Elem(null, "name", xml.Null, xml.TopScope, false, xml.Text("booyaa"))
   }
 
-  implicit val xmlGraphWriter: SimWriter[xml.Elem] = XMLGraphWriter
+  implicit val xmlSimWriter: SimWriter[xml.Elem] = this
 }

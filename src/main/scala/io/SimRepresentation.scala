cd19b5e8afdf20c44d0c14a7d0942cf81b241c88
mrRachar <matthewross@campars.net>
2020-10-15 16:17:20 +0100

    Initial model setup

---
+++ src/main/scala/io/SimRepresentation.scala
@@ -0,0 +1,23 @@
+package io
+
+import model.sim.Sim
+import XMLSimRepresentation.xmlSimRepresentation
+
+trait SimRepresentation[A] {
+  def represent(sim: Sim): A
+}
+
+// class SimRepresentation a where
+//   represent :: Sim -> A
+
+// trait SimRepresentation {
+//   fn represent(sim: Sim) -> Self
+// }
+
+object Implicits {
+  // Eq a => a -> a -> Bool
+  // SimWriter w => Sim -> w
+  implicit class RepresentableSim[W](sim: Sim)(implicit simRepresentation: SimRepresentation[W]) {
+    def toRepresentation: W = simRepresentation.represent(sim)
+  }
+}

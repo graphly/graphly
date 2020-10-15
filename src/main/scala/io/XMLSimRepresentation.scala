b1bfdff95f947e3e5a8abf40da54251eb676f5e4
TheBeggs <andrew@beggs.io>
2020-10-16 00:57:00 +0100

    Remove tutorial lines

--- src/main/scala/io/XMLSimRepresentation.scala
+++ src/main/scala/io/XMLSimRepresentation.scala
@@ -2,12 +2,6 @@ package io
 
 import model.sim.Sim
 
-// instance SimRepresentation XML.Elem where
-//   represent = xml.Elem Nothing "name" ...
-
-// impl SimRepresentation for XML::Elem {
-//   fn represent(boop: Sim) -> XML::Elem
-// }
 
 object XMLSimRepresentation extends SimRepresentation[xml.Elem] {
   override def represent(x: Sim): xml.Elem = {

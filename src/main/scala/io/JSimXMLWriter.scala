783cf984404b024ebd6a7abef46e058a70844dca
TheBeggs <andrew@beggs.io>
2020-11-12 22:29:47 +0000

    Convert writeModel to graphly

--- src/main/scala/io/JSimXMLWriter.scala
+++ src/main/scala/io/JSimXMLWriter.scala
@@ -1,5 +1,6 @@
 package io
 
+import io.XMLConstantNames._
 import javax.xml.parsers.DocumentBuilderFactory
 import model.sim.Sim
 import org.w3c.dom.Document
@@ -17,53 +18,59 @@ object JSimXMLWriter extends SimRepresentation[Document] {
     elem.setAttribute("xsi:noNamespaceSchemaLocation", XML_DOCUMENT_XSD)
     elem.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
     // Simulation seed
-    if (!model.getUseRandomSeed.booleanValue)
-      elem.setAttribute(XML_A_ROOT_SEED, model.getSimulationSeed.toString)
+    if (!model.useRandomSeed)
+      elem.setAttribute(XML_A_ROOT_SEED, model.seed.toString)
     // Max simulation time
-    if (model.getMaximumDuration.doubleValue > 0)
-      elem.setAttribute(XML_A_ROOT_DURATION, model.getMaximumDuration.toString)
+    if (model.maximumDuration > 0)
+      elem.setAttribute(XML_A_ROOT_DURATION, model.maximumDuration.toString)
     // Max simulated time (not real time, but time in system of the simulated process)
-    if (model.getMaxSimulatedTime.doubleValue > 0)
-      elem
-        .setAttribute(XML_A_ROOT_SIMULATED, model.getMaxSimulatedTime.toString)
+    if (model.maxSimulatedTime.doubleValue > 0)
+      elem.setAttribute(XML_A_ROOT_SIMULATED, model.maxSimulatedTime.toString)
     // Polling interval
-    elem.setAttribute(XML_A_ROOT_POLLING, model.getPollingInterval.toString)
+    elem.setAttribute(XML_A_ROOT_POLLING, model.pollingInterval.toString)
     // Max samples
-    elem.setAttribute(
-      XML_A_ROOT_MAXSAMPLES,
-      model.getMaxSimulationSamples.toString
-    )
+    elem.setAttribute(XML_A_ROOT_MAXSAMPLES, model.maxSamples.toString)
     // Disable statistic
     elem.setAttribute(
       XML_A_ROOT_DISABLESTATISTIC,
-      model.getDisableStatistic.toString
+      model.disableStatistic.toString
     )
     // Max events
-    elem
-      .setAttribute(XML_A_ROOT_MAXEVENTS, model.getMaxSimulationEvents.toString)
+    elem.setAttribute(XML_A_ROOT_MAXEVENTS, model.maxEvents.toString)
     // Write attributes used by the logs - Michael Fercu
     elem.setAttribute(
       XML_A_ROOT_LOGPATH,
-      MacroReplacer.replace(model.getLoggingGlbParameter("path"))
+      System.getProperty("user.home").concat("/JMT/")
+      //TODO
+//      MacroReplacer.replace(model.getLoggingGlbParameter("path"))
     )
     elem.setAttribute(
       XML_A_ROOT_LOGREPLACE,
-      model.getLoggingGlbParameter("autoAppend")
+      0.toString
+      //TODO
+//      model.getLoggingGlbParameter("autoAppend")
     )
-    elem
-      .setAttribute(XML_A_ROOT_LOGDELIM, model.getLoggingGlbParameter("delim"))
+    elem.setAttribute(
+      XML_A_ROOT_LOGDELIM,
+      ","
+      //TODO
+//        model.getLoggingGlbParameter("delim")
+    )
     elem.setAttribute(
       XML_A_ROOT_LOGDECIMALSEPARATOR,
-      model.getLoggingGlbParameter("decimalSeparator")
+      "."
+      //TODO
+//      model.getLoggingGlbParameter("decimalSeparator")
     )
     // Manage probabilities
-    model.manageProbabilities()
+    //TODO
+//    model.manageProbabilities()
     // Write all elements
     writeClasses(modelDoc, elem, model)
-    writeStations(modelDoc, elem, model)
-    writeMeasures(modelDoc, elem, model)
-    writeConnections(modelDoc, elem, model)
-    writeBlockingRegions(modelDoc, elem, model)
-    writePreload(modelDoc, elem, model)
+//    writeStations(modelDoc, elem, model)
+//    writeMeasures(modelDoc, elem, model)
+//    writeConnections(modelDoc, elem, model)
+//    writeBlockingRegions(modelDoc, elem, model)
+//    writePreload(modelDoc, elem, model)
   }
 }
--- src/main/scala/model/sim/Sim.scala
+++ src/main/scala/model/sim/Sim.scala
@@ -8,7 +8,16 @@ case class Sim(
     classes: mutable.Set[UserClass],
     measures: mutable.Set[Measure],
     traces: mutable.Buffer[Trace],
-    configuration: Sim.Configuration = mutable.HashMap.empty
+    configuration: Sim.Configuration = mutable.HashMap.empty,
+    seed: Long = 23000,
+    useRandomSeed: Boolean = true,
+    maximumDuration: Double = -1,
+    maxSimulatedTime: Double = -1,
+    maxSamples: Int = 1000000,
+    disableStatistic: Boolean = false,
+    maxEvents: Int = -1,
+    pollingInterval: Double = 1,
+    parametricAnalysisEnabled: Boolean = false
 )
 
 object Sim {

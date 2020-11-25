46704380509058f6c4df9adff7c49813c1dec08b
TheBeggs <andrew@beggs.io>
2020-11-25 19:05:40 +0000

    Add sections to NodeTypes

--- src/main/scala/model/sim/Shape.scala
+++ src/main/scala/model/sim/Shape.scala
@@ -34,63 +34,155 @@ case class Node(
 //TODO: Refactor once done, maybe a NodeType file?
 
 sealed trait NodeType
+//    TODO: Use the implemented sections
 
 case class Source(
 //    sourceSection: SourceSection,
 //    tunnelSection: TunnelSection,
 //    routerSection: RouterSection
+    sourceSection: UnimplementedSection,
+    tunnelSection: UnimplementedSection,
+    routerSection: UnimplementedSection
 ) extends NodeType
 
-case class Sink() extends NodeType
+case class Sink(
+//    sinkSection: SinkSection
+    sinkSection: UnimplementedSection
+) extends NodeType
 
-case class Terminal() extends NodeType
+case class Terminal(
+//    terminal: Terminal,
+//    tunnelSection: TunnelSection,
+//    routerSection: RouterSection
+    terminal: UnimplementedSection,
+    tunnelSection: UnimplementedSection,
+    routerSection: UnimplementedSection
+) extends NodeType
 
-case class Router() extends NodeType
+case class Router(
+//    queueSection: QueueSection,
+//    tunnelSection: TunnelSection,
+//    routerSection: RouterSection
+    queueSection: UnimplementedSection,
+    tunnelSection: UnimplementedSection,
+    routerSection: UnimplementedSection
+) extends NodeType
 
-case class Delay() extends NodeType
+case class Delay(
+//    queueSection: QueueSection,
+//    delaySection: DelaySection,
+//    routerSection: RouterSection
+    queueSection: UnimplementedSection,
+    delaySection: UnimplementedSection,
+    routerSection: UnimplementedSection
+) extends NodeType
 
 //This is what you'd intuitively call a queue
-case class Server() extends NodeType
+case class Server(
+//    queueSection: QueueSection,
+//    serverSection: ServerSection,
+//    routerSection: RouterSection
+    queueSection: UnimplementedSection,
+    serverSection: UnimplementedSection,
+    routerSection: UnimplementedSection
+) extends NodeType
 
-case class Fork() extends NodeType
+case class Fork(
+//    queueSection: QueueSection,
+//    tunnelSection: TunnelSection,
+//    forkSection: ForkSection
+    queueSection: UnimplementedSection,
+    tunnelSection: UnimplementedSection,
+    forkSection: UnimplementedSection
+) extends NodeType
 
-case class Join() extends NodeType
+case class Join(
+//    joinSection: JoinSection,
+//    tunnelSection: TunnelSection,
+//    routerSection: RouterSection
+    joinSection: UnimplementedSection,
+    tunnelSection: UnimplementedSection,
+    routerSection: UnimplementedSection
+) extends NodeType
 
-case class Logger() extends NodeType
+case class Logger(
+//    queueSection: QueueSection,
+//    loggerSection: LoggerSection,
+//    routerSection: RouterSection
+    queueSection: UnimplementedSection,
+    loggerSection: UnimplementedSection,
+    routerSection: UnimplementedSection
+) extends NodeType
 
-case class ClassSwitch() extends NodeType
+case class ClassSwitch(
+//    queueSection: QueueSection,
+//    classSwitch: ClassSwitch,
+//    routerSection: RouterSection
+    queueSection: UnimplementedSection,
+    classSwitch: UnimplementedSection,
+    routerSection: UnimplementedSection
+) extends NodeType
 
-case class Semaphore() extends NodeType
+case class Semaphore(
+//    semaphoreSection: SemaphoreSection,
+//    tunnelSection: TunnelSection,
+//    routerSection: RouterSection
+    semaphoreSection: UnimplementedSection,
+    tunnelSection: UnimplementedSection,
+    routerSection: UnimplementedSection
+) extends NodeType
 
-case class Scalar() extends NodeType
+case class Scalar(
+//    joinSection: JoinSection,
+//    tunnelSection: TunnelSection,
+//    forkSection: ForkSection
+    joinSection: UnimplementedSection,
+    tunnelSection: UnimplementedSection,
+    forkSection: UnimplementedSection
+) extends NodeType
 
-case class Place() extends NodeType
+case class Place(
+//    storageSection: StorageSection,
+//    tunnelSection: TunnelSection,
+//    linkageSection: LinkageSection
+    storageSection: UnimplementedSection,
+    tunnelSection: UnimplementedSection,
+    linkageSection: UnimplementedSection
+) extends NodeType
 
-case class Transition() extends NodeType
+case class Transition(
+//    enablingSection: EnablingSection,
+//    timingSection: TimingSection,
+//    firingSection: FiringSection
+    enablingSection: UnimplementedSection,
+    timingSection: UnimplementedSection,
+    firingSection: UnimplementedSection
+) extends NodeType
 
-case class Unimplemented() extends NodeType
+case class Unimplemented(sections: Seq[UnimplementedSection]) extends NodeType
 
 // Sections make up a NodeType
 sealed trait TypeSection
 
-case class SourceSection()      extends TypeSection
-case class TunnelSection()      extends TypeSection
-case class RouterSection()      extends TypeSection
-case class SinkSection()        extends TypeSection
-case class TerminalSection()    extends TypeSection
-case class QueueSection()       extends TypeSection
-case class DelaySection()       extends TypeSection
-case class ServerSection()      extends TypeSection
-case class ForkSection()        extends TypeSection
-case class JoinSection()        extends TypeSection
-case class LoggerSection()      extends TypeSection
-case class ClassSwitchSection() extends TypeSection
-case class SemaphoreSection()   extends TypeSection
-case class StorageSection()     extends TypeSection
-case class LinkageSection()     extends TypeSection
-case class EnablingSection()    extends TypeSection
-case class TimingSection()      extends TypeSection
-case class FiringSection()      extends TypeSection
+case class SourceSection()                     extends TypeSection
+case class TunnelSection()                     extends TypeSection
+case class RouterSection()                     extends TypeSection
+case class SinkSection()                       extends TypeSection
+case class TerminalSection()                   extends TypeSection
+case class QueueSection()                      extends TypeSection
+case class DelaySection()                      extends TypeSection
+case class ServerSection()                     extends TypeSection
+case class ForkSection()                       extends TypeSection
+case class JoinSection()                       extends TypeSection
+case class LoggerSection()                     extends TypeSection
+case class ClassSwitchSection()                extends TypeSection
+case class SemaphoreSection()                  extends TypeSection
+case class StorageSection()                    extends TypeSection
+case class LinkageSection()                    extends TypeSection
+case class EnablingSection()                   extends TypeSection
+case class TimingSection()                     extends TypeSection
+case class FiringSection()                     extends TypeSection
+case class UnimplementedSection(xml: xml.Node) extends TypeSection
 
 case class Connection(source: Node, target: Node) extends Element
 
